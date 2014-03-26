56607bf9ec3cf143b8f12404bfa49b2a2fbb5467
mbargar <mbargar@gmail.com>
2014-03-25 22:14:50 -0400

    simple build starter script is working

--- int.sh
+++ int.sh
@@ -2,4 +2,6 @@
 
 # This script acts as the command line client for integartorr.
 
-echo "hello integartorr"
+echo $1
+
+ansible-playbook -i {{ integratorr_home }}/hosts/local {{ integratorr_work }}/{{ integratorr_config }}/ansible/$1.yml
